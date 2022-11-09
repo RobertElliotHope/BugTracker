b43a609b79ada90d3f1c2e22b61e40ff207cddbf
RobertElliotHope <115725160+RobertElliotHope@users.noreply.github.com>
2022-11-09 10:18:00 +0000

    fix: login authentication to now compare both the username and password instead of either

--- src/main/java/MainLoginView.java
+++ src/main/java/MainLoginView.java
@@ -11,99 +11,99 @@ import java.sql.SQLException;
 public class MainLoginView extends JPanel implements ActionListener
 {
 
-	private final JTextField usernameField;
-	private final JPasswordField passwordField;
+    private final JTextField usernameField;
+    private final JPasswordField passwordField;
 
-	public MainLoginView()
-	{
-		// Configure panel
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		setBackground(Color.decode("#212327"));
+    public MainLoginView()
+    {
+        // Configure panel
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBackground(Color.decode("#212327"));
 
-		// Create login components
-		JLabel title = new JLabel("Login");
-		usernameField = new JTextField("Username");
-		passwordField = new JPasswordField("Password");
-		JButton loginButton = new JButton("Login");
+        // Create login components
+        JLabel title = new JLabel("Login");
+        usernameField = new JTextField("Username");
+        passwordField = new JPasswordField("Password");
+        JButton loginButton = new JButton("Login");
 
-		// Configure the title colour
-		title.setForeground(Color.WHITE);
-		title.setAlignmentX(CENTER_ALIGNMENT);
+        // Configure the title colour
+        title.setForeground(Color.WHITE);
+        title.setAlignmentX(CENTER_ALIGNMENT);
 
-		// Configure the username field
-		usernameField.setMaximumSize(new Dimension(200, 25));
-		hideTextOnFocus(usernameField);
+        // Configure the username field
+        usernameField.setMaximumSize(new Dimension(200, 25));
+        hideTextOnFocus(usernameField);
 
-		// Configure the password field
-		passwordField.setMaximumSize(new Dimension(200, 25));
-		hideTextOnFocus(passwordField);
+        // Configure the password field
+        passwordField.setMaximumSize(new Dimension(200, 25));
+        hideTextOnFocus(passwordField);
 
-		// Configure the login button
-		loginButton.setFocusable(false);
-		loginButton.setAlignmentX(CENTER_ALIGNMENT);
-		loginButton.addActionListener(this);
+        // Configure the login button
+        loginButton.setFocusable(false);
+        loginButton.setAlignmentX(CENTER_ALIGNMENT);
+        loginButton.addActionListener(this);
 
-		// Add components
-		add(Box.createVerticalGlue());
-		add(title);
-		add(Box.createVerticalStrut(5));
-		add(usernameField);
-		add(Box.createVerticalStrut(5));
-		add(passwordField);
-		add(Box.createVerticalStrut(5));
-		add(loginButton);
-		add(Box.createVerticalGlue());
-	}
+        // Add components
+        add(Box.createVerticalGlue());
+        add(title);
+        add(Box.createVerticalStrut(5));
+        add(usernameField);
+        add(Box.createVerticalStrut(5));
+        add(passwordField);
+        add(Box.createVerticalStrut(5));
+        add(loginButton);
+        add(Box.createVerticalGlue());
+    }
 
-	// Hides text in fields when focussed, saving the user the need to manually delete existing default text
-	public void hideTextOnFocus(JTextField field)
-	{
-		String prevText = field.getText();
+    // Hides text in fields when focussed, saving the user the need to manually delete existing default text
+    public void hideTextOnFocus(JTextField field)
+    {
+        String prevText = field.getText();
 
-		field.addFocusListener(new FocusListener()
-		{
+        field.addFocusListener(new FocusListener()
+        {
 
-			@Override
-			public void focusGained(FocusEvent e)
-			{
-				if (field.getText().equals(prevText)) field.setText("");
-			}
+            @Override
+            public void focusGained(FocusEvent e)
+            {
+                if (field.getText().equals(prevText)) field.setText("");
+            }
 
-			@Override
-			public void focusLost(FocusEvent e)
-			{
-				if (!field.getText().equals(prevText) && field.getText().isEmpty()) field.setText(prevText);
-			}
-		});
-	}
+            @Override
+            public void focusLost(FocusEvent e)
+            {
+                if (!field.getText().equals(prevText) && field.getText().isEmpty()) field.setText(prevText);
+            }
+        });
+    }
 
-	// Compares entered text to values in database
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		String username = usernameField.getText();
-		if (username != null && !username.isEmpty())
-		{
-			try
-			{
-				if (DatabaseUtils.compareValue(DatabaseUtils.usersURL, "Username", usernameField.getText()) &&
-						DatabaseUtils.compareValue(DatabaseUtils.usersURL, "Password", new String(passwordField.getPassword())))
-				{
-					Main.panel.loginWasSuccessful();
-				}
-				else
-				{
-					Main.panel.loginDidFail();
-				}
+    // Compares entered text to values in database
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        String username = usernameField.getText();
+        if (username != null && !username.isEmpty())
+        {
+            try
+            {
+                if (DatabaseUtils.compareValue(DatabaseUtils.usersURL, "Username", "Password", usernameField.getText(), new String(passwordField.getPassword())))
+                {
+                    Main.panel.loginWasSuccessful();
+                }
+                else
+                {
+                    Main.panel.loginDidFail();
+                }
 
-			} catch (SQLException ex)
-			{
-				throw new RuntimeException(ex);
-			}
-		}
-		else
-		{
-			Main.panel.loginDidFail();
-		}
-	}
+            }
+            catch (SQLException ex)
+            {
+                throw new RuntimeException(ex);
+            }
+        }
+        else
+        {
+            Main.panel.loginDidFail();
+        }
+    }
 }
--- src/main/java/database/DatabaseUtils.java
+++ src/main/java/database/DatabaseUtils.java
@@ -6,58 +6,60 @@ import java.sql.*;
 public final class DatabaseUtils
 {
 
-	public static final String usersURL = "jdbc:sqlite:users.db";
+    public static final String usersURL = "jdbc:sqlite:users.db";
 
-	// TODO: Make this a table, and not a separate database
-	public static final String bugsURL = "jdbc:sqlite:bugs.db";
+    // TODO: Make this a table, and not a separate database
+    public static final String bugsURL = "jdbc:sqlite:bugs.db";
 
-	public static void init() throws SQLException
-	{
-		// Create users table
-		connect(usersURL, "CREATE TABLE IF NOT EXISTS users(UserID INTEGER PRIMARY KEY, Username varchar(25)," +
-				"Password varchar(25), IsAdmin BIT)");
+    public static void init() throws SQLException
+    {
+        // Create users table
+        connect(usersURL, "CREATE TABLE IF NOT EXISTS users(UserID INTEGER PRIMARY KEY, Username varchar(25)," +
+            "Password varchar(25), IsAdmin BIT)");
 
-		// Register default test account credentials
-		registerAccount("Username", "Password", true);
-	}
+        // Register default test account credentials
+        registerAccount("Username", "Password", true);
+        registerAccount("Username2", "Password2", true);
+    }
 
-	// Connect to database and optionally execute chosen statement
-	public static void connect(String databaseURL, String... sql) throws SQLException
-	{
-		File file = new File(databaseURL);
+    // Connect to database and optionally execute chosen statement
+    public static void connect(String databaseURL, String... sql) throws SQLException
+    {
+        File file = new File(databaseURL);
 
-		try (Connection conn = DriverManager.getConnection(databaseURL))
-		{
-			Statement statement = conn.createStatement();
+        try (Connection conn = DriverManager.getConnection(databaseURL))
+        {
+            Statement statement = conn.createStatement();
 
-			if (!file.exists())
-			{
-				statement.executeUpdate(sql[0]);
-			}
-		}
-	}
+            if (!file.exists())
+            {
+                statement.executeUpdate(sql[0]);
+            }
+        }
+    }
 
-	// Register an account
-	public static void registerAccount(String username, String password, boolean isAdmin) throws SQLException
-	{
-		connect(usersURL, "INSERT INTO users (Username,Password,IsAdmin) VALUES ('" + username + "'," +
-				" '" + password + "', " + (isAdmin ? 1 : 0) + ")"); // Connect and convert java boolean input to sql BIT
-	}
+    // Register an account
+    public static void registerAccount(String username, String password, boolean isAdmin) throws SQLException
+    {
+        connect(usersURL, "INSERT INTO users (Username,Password,IsAdmin) VALUES ('" + username + "'," +
+            " '" + password + "', " + (isAdmin ? 1 : 0) + ")"); // Connect and convert java boolean input to sql BIT
+    }
 
-	// Check a value against a specified input
-	public static boolean compareValue(String databaseURL, String columnLabel, String input) throws SQLException
-	{
-		try (Connection conn = DriverManager.getConnection(databaseURL))
-		{
-			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE " + columnLabel + " = '" + input + "'");
+    // Check a value against a specified input
+    public static boolean compareValue(String databaseURL, String usernameField, String passwordField, String username, String password) throws SQLException
+    {
+        try (Connection conn = DriverManager.getConnection(databaseURL))
+        {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE " + usernameField + " = '" + username + "' AND " + passwordField + " = '" + password + "'");
 
-			if (resultSet.next())
-			{
-				return true;
-			}
-		}
+            if (resultSet.next())
+            {
+                return true;
+            }
 
-		return false;
-	}
+        }
+
+        return false;
+    }
 }
